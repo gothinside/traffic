3db658e8d52f3cf7fc0ba87acf8f956236a54d6b
gothinside <kot_dok@list.ru>
2024-11-24 14:54:54 +0300

    change_env

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
+from settings import DB_URL
 
 
-engine = create_async_engine("sqlite+aiosqlite:///traffic_db.db", future=True, echo=True)
+engine = create_async_engine(f"sqlite+aio{DB_URL}", future=True, echo=True)
 
 
 Base = declarative_base()
